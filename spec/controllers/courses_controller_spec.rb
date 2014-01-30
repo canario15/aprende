2d6c24ca77017b2241adf8ded2af5fb3ef5ec496
Gaston Arbeletche -VAIRIX- <garbeletche@vairix.com>
2014-01-30 16:49:14 -0200

    Modified all test in courses_controller CR:GA

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -6,29 +6,31 @@ describe CoursesController do
   end
 
   describe "GET 'index'" do
-    it "returns http success" do
+    it "returns http success(code=200)" do
       get 'index'
-      expect(response).to be_success
+      expect(response.code).to eq("200")
     end
   end
 
   describe "GET 'new'" do
     it "returns http success" do
       get 'new'
-      expect(response).to be_success
+      expect(response.code).to eq("200")
     end
   end
 
   describe "POST 'create'" do
-
-    it "create a new course" do
-      get 'create', {:course => {:title => "new course", :level_id => @level.id}}
-      change{Course.count}.by(1)
+    before :each do
+      @params = {:course => {:title => "new course", :level_id => @level.id}}
     end
 
-    it "returns http success" do
-      post 'create', {:course => {:title => "new course", :level_id => @level.id}}
-      expect(response) == 1
+    it "create a new course" do
+      expect {post('create', @params)}.to change{Course.count}.by(1)
+    end
+
+    it "create course and returns http courses_url" do
+      post 'create', @params
+      expect(response).to redirect_to(courses_url)
     end
   end
 
@@ -37,9 +39,9 @@ describe CoursesController do
       @course = Course.make!
     end
 
-    it "returns http success" do
+    it "returns http success(code=200)" do
       get 'edit', {:id => @course.id}
-      expect(response).to be_success
+      expect(response.code).to eq("200")
     end
   end
 
@@ -48,9 +50,10 @@ describe CoursesController do
       @course = Course.make!
     end
 
-    it "returns http success" do
-      post 'update', {:id=> @course.id, :course => {:title => "Update course", :level_id => @level.id}}
-      expect(response) == 1
+    it "returns http success and redirect_to courses_url" do
+      params = {:id=> @course.id, :course => {:title => "Update course", :level_id => @level.id}}
+      post 'update', params
+      expect(response).to redirect_to(courses_url)
     end
   end
 
