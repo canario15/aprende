9c5816b44bc19122940313dd76aaff56ed08dab3
Esteban Fernandez <efernandez@vairix.com>
2014-03-18 10:31:49 -0300

    Fixed: -remove flash.now CR= MV

--- app/controllers/teachers/sessions_controller.rb
+++ app/controllers/teachers/sessions_controller.rb
@@ -3,7 +3,7 @@ class Teachers::SessionsController < Devise::SessionsController
   def create
     teacher = Teacher.find_by(email: params[:teacher][:email])
     if teacher.try(:inactive)
-      flash.now[:alert] = t 'devise.failure.inactive_by_admin'
+      flash[:alert] = t 'devise.failure.inactive_by_admin'
       redirect_to new_teacher_session_path
     else
       super
