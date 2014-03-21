9dc0444b73477b38df4f9341e8bf8f2c7bd5842d
Esteban Fernandez <efernandez@vairix.com>
2014-03-21 14:38:09 -0300

    Template first commit

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(:default, Rails.env)
 module Aprende
   class Application < Rails::Application
     require 'constants'
+    config.assets.paths << Rails.root.join('app', 'assets', 'font')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
